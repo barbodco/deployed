beb8f3c1a838029836252e36ce22f24296ed6d55
barbodco <kp.savadi@gmail.com>
2016-02-03 22:52:32 +0800

    FUCK YOU,  WORKKKK

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var express = require('express');  
-var app = express();
+gzippo = require 'gzippo'
+express = require 'express'
 
-app.use(express.static(__dirname)); // Current directory is root  
-app.listen(process.env.PORT || 5000);  
+app = express()
+app.use express.logger 'dev'
+app.use gzippo.staticGzip "#{__dirname}/dist"
+app.listen process.env.PORT || 5000
