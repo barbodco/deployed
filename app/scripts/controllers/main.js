1c946e1cdb1ef4332325425772c918b920c29863
barbodco <kp.savadi@gmail.com>
2016-02-06 20:02:07 +0800

    First json call

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,19 @@
  * Controller of the inventoryApp
  */
 angular.module('inventoryApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope,$http) {
+    
+    $scope.headers = 'test';
+
+    $http({
+		  method: 'GET',
+		  url: 'http://dvcjson.somee.com/home/saeidjson'
+			}).then(function successCallback(response) {
+			    // this callback will be called asynchronously
+			    // when the response is available
+			    console.log(response);
+			  }, function errorCallback(response) {
+			    // called asynchronously if an error occurs
+			    // or server returns response with an error status.
+	  });
   });
