4cdbcb0a16ab5ebaabbace8c08858a0a373d0bf5
barbodco <kp.savadi@gmail.com>
2016-02-07 03:17:45 +0800

    Retrieve data from sample json phase1

--- .tmp/concat/scripts/scripts.js
+++ .tmp/concat/scripts/scripts.js
@@ -43,27 +43,51 @@ angular
  */
 angular.module('inventoryApp')
   .controller('MainCtrl', ["$scope", "$http", function ($scope,$http) {
-    
-    $scope.awesomeThings = [
+  	$scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
 
-    $http({
-		  method: 'GET',
-		  url: 'http://dvcjson.somee.com/home/saeidjson'
-			}).then(function successCallback(response) {
-			    // this callback will be called asynchronously
-			    // when the response is available
-			    console.log(response);
-			  }, function errorCallback(response) {
-			    // called asynchronously if an error occurs
-			    // or server returns response with an error status.
-			    console.log(response);
-	  });
-  }]);
+    $scope.attrs = [
+        { attr: 'type-checkbox', value: '' }
+    ];
+  $scope.mainData = '';
+  $scope.showColumn = {};
+  $scope.limitation = 10;
+  $scope.typeControl = {};
+  $http.get('table.json')
+  	.then(function(response){
+  		$scope.mainData = response.data.GD;
+  		$scope.limitation = $scope.mainData.rowsperpage;
+  	});
 
+  }]);
+angular.module('inventoryApp').directive('typeCheckbox', function() {
+	  return {
+	    scope: {
+	      typeCheckox: '=' //import referenced model to our directives scope
+	    },
+	    templateUrl: 'checkbox.html',
+	    // link: function(scope, elem, attr, ctrl) {
+	      
+	    //   scope.$watch('demoDisplay', function() { // watch for when model changes
+
+	    //     elem.html("") //remove all elements
+
+	    //     angular.forEach(scope.demoDisplay, function(d) { //iterate list
+	    //       var s = scope.$new(); //create a new scope
+	    //       angular.extend(s, d); //copy data onto it
+	    //       console.log(scope.demoDays);
+
+	    //       var template = '';
+	    //       elem.append($compile(template)(s)); // compile template & append
+	    //     });
+	    //   }, true) //look deep into object
+	      
+	    // }
+	  };
+	});
 'use strict';
 
 /**
